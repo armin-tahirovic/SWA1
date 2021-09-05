9e959ad09f2fe6a98536b537b9ef0a96222b7afb
armin tahirovic <armin2013@live.dk>
2021-09-05 15:43:37 +0200

    Exercise 1.2 done

--- Temperature.js
+++ Temperature.js
@@ -54,8 +54,10 @@ function temperaturePrediction(unit, minValue, maxValue) {
     }
 
     function matches(temp) {
-        if(temp >= minValue || temp <= maxValue) {
+        if(temp >= minValue && temp <= maxValue) {
             return true
+        } else {
+            return false
         }
     }
 
@@ -84,7 +86,7 @@ function temperaturePrediction(unit, minValue, maxValue) {
 }
 
 let temp3 = temperaturePrediction("C", 15, 25)
-console.log(temp3.matches(20))
+console.log(temp3.matches(30))
 
 
 function precipitation(unit, value, precipitationType) {
@@ -123,3 +125,164 @@ function precipitation(unit, value, precipitationType) {
         convertToMM
     }
 }
+
+let temp4 = precipitation("MM", 78, "Rain")
+temp4.convertToInches()
+console.log(temp4.getUnit(), temp4.getValue())
+
+
+function precipitationPrediction(unit, minValue, maxValue, expectedType) {
+
+    function getUnit() {
+        return unit
+    }
+
+    function getMinValue() {
+        return minValue
+    }
+
+    function getMaxValue() {
+        return maxValue
+    }
+
+    function getExpectedType() {
+        return expectedType
+    }
+
+    function matches(precipitation) {
+        if(precipitation >= minValue && precipitation <= maxValue) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function convertToInches() {
+        if(unit === "MM") {
+            value = value * 0.039370
+            unit = "Inch"
+        }
+    }
+
+    function convertToMM() {
+        if(unit === "Inch") {
+            value = value * 25.4
+            unit = "MM"
+        }
+    }
+
+    return {
+        getUnit,
+        getMinValue,
+        getMaxValue,
+        getExpectedType,
+        matches,
+        convertToInches,
+        convertToMM
+    }
+}
+
+let temp5 = precipitationPrediction("Inch", 1, 5, "Snow")
+console.log(temp5.matches(3))
+console.log(temp5.matches(10))
+
+
+function wind(unit, value, direction) {
+
+    function getUnit() {
+        return unit
+    }
+
+    function getValue() {
+        return value
+    }
+
+    function getDirection() {
+        return direction
+    }
+
+    function convertToMPH() {
+        if(unit === "MS") {
+            value = value * 2.236936
+            unit = "MPH"
+        }
+    }
+
+    function convertToMS() {
+        if(unit === "MPH") {
+            value = value * 0.44704
+            unit = "MS"
+        }
+    }
+
+    return {
+        getUnit,
+        getValue,
+        getDirection,
+        convertToMPH,
+        convertToMS
+    }
+}
+
+let temp6 = wind("MS", 5, "North")
+temp6.convertToMPH()
+let temp7 = wind("MPH", 5, "West")
+temp7.convertToMS()
+console.log(temp6.getValue(), temp6.getUnit())
+console.log(temp7.getValue(), temp7.getUnit())
+
+
+function windPrediction(unit, minValue, maxValue, expectedDirection) {
+
+    function getUnit() {
+        return unit
+    }
+
+    function getMinValue() {
+        return minValue
+    }
+
+    function getMaxValue() {
+        return maxValue
+    }
+
+    function getExpectedDirection() {
+        return expectedDirection
+    }
+
+    function matches(wind) {
+        if(wind >= minValue && wind <= maxValue) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function convertToMPH() {
+        if(unit === "MS") {
+            value = value * 2.236936
+            unit = "MPH"
+        }
+    }
+
+    function convertToMS() {
+        if(unit === "MPH") {
+            value = value * 0.44704
+            unit = "MS"
+        }
+    }
+
+    return {
+        getUnit,
+        getMinValue,
+        getMaxValue,
+        getExpectedDirection,
+        matches,
+        convertToMPH,
+        convertToMS
+    }
+}
+
+let temp8 = windPrediction("MS", 5, 15, "East")
+console.log(temp8.matches(3))
+console.log(temp8.matches(10))
