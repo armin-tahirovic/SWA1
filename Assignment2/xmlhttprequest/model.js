dfd1972f10a693fad3ec5c7929638f451a09d17d
armin tahirovic <armin2013@live.dk>
2021-10-11 22:40:29 +0200

    done with xmlhttprequest

--- Assignment2/xmlhttprequest/model.js
+++ Assignment2/xmlhttprequest/model.js
@@ -1,10 +1,117 @@
-const model = (weathers, filter = () => true) => {
-    const weatherData = () => weathers
-    .map(w => ({...w}))
-    .filter(filter)
+const model = (data, forecast) => {
 
-    const filtered = filter => model(weathers, filter)
-    const all = () => model(weathers)
+    const latestMeasurement = () => data
+        .sort((a, b) => (a.time > b.time) ? -1 : 1)
+        .filter(point => point.place === "Copenhagen")
+        .slice(0, 4)
+        .concat(data.sort((a, b) => (a.time > b.time) ? -1 : 1)
+            .filter(point => point.place === "Horsens")
+            .slice(0, 4))
+        .concat(data.sort((a, b) => (a.time > b.time) ? -1 : 1)
+            .filter(point => point.place === "Aarhus")
+            .slice(0, 4))
 
-    return {weatherData, filtered, all}
+    const minTemp = () => data
+        .filter(point => point.place === "Copenhagen")
+        .filter(point => point.type === "temperature")
+        .sort((a, b) => (a.value > b.value) ? 1 : -1)
+        .slice(0, 1)
+        .concat(data
+            .filter(point => point.place === "Horsens")
+            .filter(point => point.type === "temperature")
+            .sort((a, b) => (a.value > b.value) ? 1 : -1)
+            .slice(0, 1))
+        .concat(data
+            .filter(point => point.place === "Aarhus")
+            .filter(point => point.type === "temperature")
+            .sort((a, b) => (a.value > b.value) ? 1 : -1)
+            .slice(0, 1))
+
+    const maxTemp = () => data
+        .filter(point => point.place === "Copenhagen")
+        .filter(point => point.type === "temperature")
+        .sort((a, b) => (a.value > b.value) ? -1 : 1)
+        .slice(0, 1)
+        .concat(data
+            .filter(point => point.place === "Horsens")
+            .filter(point => point.type === "temperature")
+            .sort((a, b) => (a.value > b.value) ? -1 : 1)
+            .slice(0, 1))
+        .concat(data
+            .filter(point => point.place === "Aarhus")
+            .filter(point => point.type === "temperature")
+            .sort((a, b) => (a.value > b.value) ? -1 : 1)
+            .slice(0, 1))
+
+
+    const totalPrecipitation = () => [data
+        .filter(point => point.place === "Copenhagen")
+        .filter(point => point.type === "precipitation")
+        .slice(0, 120)
+        .reduce((s, n) => ({ value: s.value + n.value, place: "Copenhagen" }))]
+        .concat([data
+            .filter(point => point.place === "Horsens")
+            .filter(point => point.type === "precipitation")
+            .slice(0, 120)
+            .reduce((s, n) => ({ value: s.value + n.value, place: "Horsens" }))]
+        )
+        .concat([data
+            .filter(point => point.place === "Aarhus")
+            .filter(point => point.type === "precipitation")
+            .slice(0, 120)
+            .reduce((s, n) => ({ value: s.value + n.value, place: "Aarhus" }))]
+        )
+
+    const avgWindspeed = () => [data
+        .filter(point => point.place === "Copenhagen")
+        .filter(point => point.type === "wind speed")
+        .slice(0, 120)
+        .reduce((s, n) => ({
+            value: s.value + n.value /
+                data.filter(point => point.place === "Copenhagen").filter(point => point.type === "wind speed").length
+            , place: "Copenhagen"
+        }))]
+        .concat([data
+            .filter(point => point.place === "Horsens")
+            .filter(point => point.type === "wind speed")
+            .slice(0, 120)
+            .reduce((s, n) => ({
+                value: s.value + n.value /
+                    data.filter(point => point.place === "Horsens").filter(point => point.type === "wind speed").length
+                , place: "Horsens"
+            }))]
+        )
+        .concat([data
+            .filter(point => point.place === "Aarhus")
+            .filter(point => point.type === "wind speed")
+            .slice(0, 120)
+            .reduce((s, n) => ({
+                value: s.value + n.value /
+                    data.filter(point => point.place === "Aarhus").filter(point => point.type === "wind speed").length
+                , place: "Aarhus"
+            }))]
+        )
+
+    const hourlyPrediction = () => forecast
+        .sort((a, b) => (a.time > b.time) ? 1 : -1)
+        .filter(point => point.place === "Copenhagen")
+        .slice(0, 96)
+        .reverse()
+        .sort((a, b) => (a.type > b.type) ? 1 : -1)
+        .concat(data.sort((a, b) => (a.time > b.time) ? 1 : -1)
+            .filter(point => point.place === "Horsens")
+            .slice(0, 96)
+            .reverse()
+            .sort((a, b) => (a.type > b.type) ? 1 : -1)
+        )
+        .concat(data.sort((a, b) => (a.time > b.time) ? 1 : -1)
+            .filter(point => point.place === "Aarhus")
+            .slice(0, 96)
+            .reverse()
+            .sort((a, b) => (a.type > b.type) ? 1 : -1)
+        )
+
+    const allData = () => data.slice(0, 10)
+
+    return { latestMeasurement, minTemp, maxTemp, totalPrecipitation, avgWindspeed, hourlyPrediction, allData }
 }
--- Assignment2/xmlhttprequest/view.js
+++ Assignment2/xmlhttprequest/view.js
@@ -1,8 +1,8 @@
 const view = (window) => {
     const document = window.document
-    const table_body = document.getElementById('WeatherData')
+    const tables = ['measurement', 'showMinData', 'showMaxData', 'showTotalData', 'showAvgData', 'showPredictData']
 
-    const addweatherData = w => {
+    const addData = w => {
         const tr = table_body.appendChild(document.createElement('tr'))
         tr.insertCell().appendChild(document.createTextNode(w.value))
         tr.insertCell().appendChild(document.createTextNode(w.type))
@@ -13,17 +13,59 @@ const view = (window) => {
         if (w.direction) tr.insertCell().appendChild(document.createTextNode(w.direction))
     }
 
+    const addRow = r => {
+        const tr = table_body.appendChild(document.createElement('tr'))
+        tr.insertCell().appendChild(document.createTextNode(r.value))
+        tr.insertCell().appendChild(document.createTextNode(r.place))
+    }
+
     const displayError = e => {
         const msg_board = document.getElementById('error message')
         msg_board.innerText = e
     }
 
+    const updateTable = (model, item) => {
+        table_body = document.getElementById(item)
+
+        switch (item) {
+            case 'measurement':
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.latestMeasurement().forEach(addData)
+                break;
+            case 'showMinData':
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.minTemp().forEach(addData)
+                break;
+            case 'showMaxData':
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.maxTemp().forEach(addData)
+                break;
+            case "showTotalData":
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.totalPrecipitation().forEach(addRow)
+                break;
+            case "showAvgData":
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.avgWindspeed().forEach(addRow)
+                break;
+
+            case "showPredictData":
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.hourlyPrediction().forEach(addData)
+                break;
+
+            default:
+                while (table_body.firstChild) table_body.removeChild(table_body.firstChild)
+                model.allData().forEach(addData)
+                break;
+        }
+    }
+
     const update = model => {
-        while(table_body.firstChild) table_body.removeChild(table_body.firstChild)
-        model.weatherData().forEach(addweatherData)
+        tables.forEach(updateTable.bind(null, model))
     }
 
     const prompt = window.prompt.bind(window)
 
-    return { addweatherData, update, prompt, displayError}
+    return { addData, update, prompt, displayError }
 }
